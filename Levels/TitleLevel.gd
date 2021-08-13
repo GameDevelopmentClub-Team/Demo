e1a864c7619ac46bdb022c3cb47a0af1d102ba29
qwerty111i <adwaithsramesh@gmail.com>
2021-08-13 15:28:07 -0400

    Removed some files

--- Levels/TitleLevel.gd
+++ Levels/TitleLevel.gd
@@ -1,9 +1,5 @@
 extends Node2D
 	
-signal level_changed
-
-export (String) var level_name = "Title"
-	
 func _process(delta):
 	if $TitleMusic.playing == false:
 		$TitleMusic.play()
@@ -16,7 +12,6 @@ func _on_Settings_pressed():
 	get_tree().change_scene("res://Levels/SettingsLevel.tscn")
 	
 func _unhandled_key_input(event) -> void:
-	emit_signal("level_changed", level_name)
 	$ButtonSound.play()
 	yield($ButtonSound, "finished")
 	get_tree().change_scene("res://Levels/EasyMazeLevel.tscn")
